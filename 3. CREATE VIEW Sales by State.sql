b1263aa43a6f078ef7f272b1b029eb1f0bbf476c
BunceMichael <108441653+BunceMichael@users.noreply.github.com>
2023-05-22 15:16:07 -0400

    Update 3. CREATE VIEW Sales by State.sql

--- 3. CREATE VIEW Sales by State.sql
+++ 3. CREATE VIEW Sales by State.sql
@@ -2,11 +2,11 @@
 --This one aggregates data by state for geographic sales analysis
 CREATE VIEW Sales_by_State 
 AS 
-SELECT C.state as State, COUNT(DISTINCT I.order_id) AS Total_Orders, ROUND(SUM(I.line_total),0) AS Revenue_in_USD 
-FROM order_items I
-INNER JOIN orders O ON O.order_id = I.order_id
-INNER JOIN customers C on O.customer_id = C.customer_id
-GROUP BY C.state;
+  SELECT C.state as State, COUNT(DISTINCT I.order_id) AS Total_Orders, ROUND(SUM(I.line_total),0) AS Revenue_in_USD 
+  FROM order_items I
+  INNER JOIN orders O ON O.order_id = I.order_id
+  INNER JOIN customers C on O.customer_id = C.customer_id
+  GROUP BY C.state;
 
 --Query the view
 SELECT * FROM [dbo].[Sales_by_State];
