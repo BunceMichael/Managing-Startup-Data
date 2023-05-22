a819d4d02f1e60ae926cf8c55ca561c96a934bb9
BunceMichael <108441653+BunceMichael@users.noreply.github.com>
2023-05-21 23:20:41 -0400

    Update CREATE VIEW Customer Info.sql

--- CREATE VIEW Customer Info.sql
+++ CREATE VIEW Customer Info.sql
@@ -1,4 +1,7 @@
+--Create a view so information can be viewed/interacted with safely, without risking the data in the database
 CREATE VIEW Customer_Info
 AS
 SELECT customer_id, first_name, last_name, email, city, state
 FROM customers
+
+--This can be manipulated without impacting the database
